a164ee25af2b8878c8de3ae69b852113c7b13890
petar <evgenievp@yahoo.com>
2024-12-25 11:29:43 +0200

    aws settings

--- web_page/settings.py
+++ web_page/settings.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 mimetypes.add_type("text/css", ".css", True)
 load_dotenv()
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = os.environ.get("DEBUG", "False").split(",")
@@ -74,14 +75,15 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'web-portfolio',
-        'USER': 'evgenievp',
-        'PASSWORD': 'Boisdechapp7.',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
